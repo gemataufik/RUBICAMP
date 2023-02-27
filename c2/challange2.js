c40d8904e2806dd8fc94262dc41168b6368dc30e
gemaTaufik <gemataufik19@gmail.com>
2023-02-27 23:15:58 +0700

    challange 2 selesai

---
+++ c2/challange2.js
@@ -0,0 +1,15 @@
+function deretKaskus (n){
+  let result = []
+  for (let i = 3; i <= 3*n; i+=3){
+    if (i % 5 == 0 && i % 6 == 0)
+    result.push("KASKUS")
+    else if (i % 5 == 0)
+    result.push("KAS")
+    else if (i % 6 == 0)
+    result.push("KUS")
+    else
+    result.push(i)
+  }
+  return result
+};
+console.log(deretKaskus(10))
