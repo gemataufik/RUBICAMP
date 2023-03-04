2252db17fe2f2386f70725b400e5bfc45da2df8c
gemaTaufik <gemataufik19@gmail.com>
2023-03-04 15:46:20 +0700

    tugas 5 selesai

--- c5/challange5.js
+++ c5/challange5.js
@@ -14,47 +14,9 @@ function stringManipulation(word) {
   } else if (variabel == vokal[4]) {
     console.log(word)
   } else {
-    console.log(word.substring(1)+ variabel + "nyo")
+    console.log(word.substring(1) + variabel + "nyo")
   }
 
 }
 stringManipulation('ayam')
 stringManipulation('bebek')
-
-
-
-
-
-
-
-
-
-// function stringManipulation(word) {
-//   let vokal = ['a', 'i', 'u', 'e', 'o'];
-//   let variabel = word.charAt(0)
-//   let result = '';
-
-//   switch (result) {
-
-//     case variabel === vokal.length:
-//       console.log(word)
-//       break;
-    // case variabel === vokal[1]:
-    //   console.log(word)
-    //   break;
-    // case variabel === vokal[2]:
-    //   console.log(word)
-    //   break;
-    // case variabel === vokal[3]:
-    //   console.log(word)
-    //   break;
-    // case variabel === vokal[4]:
-    //   console.log(word)
-    //   break;
-//      default :
-//       console.log(word.substring(1)+ variabel + "nyo")
-//       break;
-//   }
-// }
-// stringManipulation('ayam')
-// stringManipulation('bebek')
